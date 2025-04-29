b16194546c68a68ad86dfce5b4effabfdbc94cd2
xumomobaba <979164819@qq.com>
2025-04-29 15:52:06 +0800

    初始化

---
+++ src/main/java/org/example/hello_di/HelloDiApplication.java
@@ -0,0 +1,13 @@
+package org.example.hello_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hello_di/HelloDiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hello_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
